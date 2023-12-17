90e11ec45e38a6be84797ba003d7c3199c8afe20
Giacomo Curatella <giacur@MacBook-Air-di-Giacomo.local>
2023-12-17 22:28:24 +0100

    fetch fatta e funzionante con for delle cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,40 @@
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
 
+  const [list , setList] = useState([]);
+
+  useEffect( ()=>{
+      for (let index = 1; index < 13; index++) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${index}/`;
+        const getPokemon = async ()=>{
+            const response = await fetch(url);
+            const data = await response.json();
+            setList(prevState=>{
+              return [
+                ...prevState, data
+              ]
+            })
+        }
+
+        getPokemon();
+    }
+  }, []);
+
+  // console.log(list)
+
+
   return (
     <>
-      
+      <section className='container mx-auto flex flex-wrap'>
+        {list.map(pokemon=>{
+          return  <article key={pokemon.name} className='w-1/4 shadow-md p-4 flex flex-col justify-center'>
+                    <img src={pokemon.sprites.other.dream_world.front_default} alt="" className='w-full h-2/3'/>
+                    <h2 className='text-center font-bold italic capitalize mt-5 text-2xl'>{pokemon.name}</h2>
+                  </article>
+        })}
+      </section>
     </>
   )
 }
